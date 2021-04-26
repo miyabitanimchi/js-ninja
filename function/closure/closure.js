59b8900d247024b8a42d5fb08c5c16a4f49b2aa8
miyabitanimchi <69213541+miyabitanimchi@users.noreply.github.com>
2021-04-26 12:34:30 -0700

    add example code

--- function/closure/closure.js
+++ function/closure/closure.js
@@ -21,4 +21,51 @@
 // A: "A closure is the combination of a function bundled together (enclosed) with references 
 //    to its surrounding state (the lexical environment)." 
 //    by MDN: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Closures
-// A: 産みの親(と祖先全員)が持っている変数と関数の組み合わせ(レキシカル環境)をクロージャと呼ぶ。
+// A: 関数と、産みの親(と祖先全員)が持っている変数の組み合わせ(レキシカル環境)をクロージャと呼ぶ。
+
+
+
+
+// (Lexical scope)
+var x = 100;
+function testA() { // The scope of x is determined when testA() function is defined, and it keeps the scope.
+
+  console.log(x);
+}
+
+function testB() {
+  var x = 200; // Even though x is redefined, testA() still keeps the global variable x.
+  testA();
+}
+
+console.log(testA()); // 100
+console.log(testB()); // 100
+
+
+
+
+/* How the closure works */
+function funcA() {
+  var foo = "foo!";
+  console.log(foo); // This is executed by funcA() from "var myFunc = funcA()"
+
+  function funcB() {
+    alert(foo);
+  }
+  return funcB;  // closure, this will be assigned to the global variable below
+}
+
+var myFunc = funcA(); 
+// closure (funcB is assigned to the global variable myFunc)
+// funcA is executed only when myFunc is defined
+
+myFunc(); // THIS ONLY EXECUTES THE ALERT
+myFunc();
+
+// Result of this code
+// alert foo!
+// alert foo!
+// Output to console foo! 
+
+// In this case, the function funcB and the (lexical) environment when funcB was defined
+// are defined as the closure.
