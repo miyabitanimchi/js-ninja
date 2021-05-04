3fb9279901f163629b6af9b11274d09322b999aa
miyabitanimchi <69213541+miyabitanimchi@users.noreply.github.com>
2021-05-04 01:29:31 -0700

    add some explanations and example code

---
+++ dom/querySelector_querySelectorAll/querySelector_querySelectorAll.js
@@ -0,0 +1,21 @@
+/* querySelector, querySelectorAll / May the 4th be with you! 2021 */
+
+
+/* What is querySelector? */
+// The querySelector() method returns the FIRST element that matches a specified CSS selector(s) in the document.
+// by W3schools
+// The querySelectorAll() method returns all elements in the document that matches a specified CSS selector(s), 
+// as a static NodeList object.
+// by W3schools
+
+
+/* [querySelector] More syntax I need to learn */
+
+// 1. document.querySelector("p.highlight");
+// Get the first <p> element in the document with class="highlight":
+
+// 2. document.querySelector("div > img");
+// Get the first <img> element in the document where the parent is a <div> element.
+
+// 3. document.querySelector("button[type]");
+// Get the first <button> element in the document that has a "type" attribute:
